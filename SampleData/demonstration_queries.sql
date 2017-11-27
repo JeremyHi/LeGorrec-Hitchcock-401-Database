3b6bc1eedb7b15f5c2ee4c06484b503d3bda27fb
Jeremy <jhitchco@lion.lmu.edu>
2017-11-27 12:18:41 -0800

    added sample demonsgtration queries

--- SampleData/demonstration_queries.sql
+++ SampleData/demonstration_queries.sql
@@ -1,9 +1,16 @@
--- * A list of the posts for each user by date.
-
--- * An ability to search the art in our databases and have the results displayed in a list with related post data.
-
--- * An ability to search the artists(Users) in our databases and have the results displayed in a list with related post data.  
-
--- * A specific artist's page with a list of work the artist has for sale.
-
--- * Get each post that the User has on the site.
+-- * A list of the posts each user has.
+SELECT "Users".username,"Post".post_title,"Post".post_description,"Post".post_owner 
+	FROM "Users","Post" 
+	INNER JOIN "Users" 
+	AS J1 ON ("Post".post_owner = J1.id);
+-- * Security to not have users plaintext passwords displayed.
+SELECT username,password FROM "Users";
+-- * Ability to insert new user manually into table.  
+INSERT INTO "Users" (id,username,email,password,"createdAt","updatedAt") 
+	VALUES (33,'Lil Peep','Bake@woodlee.com','password403',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+-- * Create a post for a user manually.
+INSERT INTO "Post" (id,post_num,post_title,post_price,post_description,post_photo,post_location,post_owner,"createdAt","updatedAt") 
+	VALUES (62,61,'Devil Punisher',112211.56,'The only gun of Brados from God of War','photo.mem','Bathens,Greece',2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+-- * Average Price of items on table (marketing purposes).
+SELECT AVG(post_price) from "Post";
+	
